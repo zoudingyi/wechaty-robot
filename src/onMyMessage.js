8e81818183c7037bf2c3d0b5e6214584e747a78f
zoudingyi <541363353@qq.com>
2020-07-08 09:39:54 +0800

    合并冲突

--- src/onMyMessage.js
+++ src/onMyMessage.js
@@ -3,14 +3,22 @@
  * @Author: zdy
  * @Date: 2020-07-03 15:45:42
  * @LastEditors: zdy
- * @LastEditTime: 2020-07-03 17:46:28
+ * @LastEditTime: 2020-07-07 17:57:59
  */ 
 const { Message } = require("wechaty")
+const { FileBox } = require('file-box')
 
 let isAuto = true // 自动回复模式
 // const WeChatId = 'superegg177'
 const WeChatId = 'r-a-n-son-g'
 
+const imgs = [
+  'http://hbimg.b0.upaiyun.com/11bad3bf0d9976e774e442aff23b6d2d1d276ddb933f-6vxpJs_fw236',
+  'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1594116772774&di=9feace42b3538216974d31cf649e3783&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201806%2F02%2F20180602195147_oswdx.jpeg',
+  'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1898554905,156454153&fm=26&gp=0.jpg',
+  'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3362606401,1751985198&fm=26&gp=0.jpg',
+  'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2832867884,2827131253&fm=26&gp=0.jpg'
+]
 module.exports = bot => {
   return async function onMessage(msg) {
     consoleInfo(msg)
@@ -26,15 +34,37 @@ module.exports = bot => {
     }
     
     if (!isAuto) return
-    if (msg.from().id !== WeChatId) return
+    // if (msg.from().id !== WeChatId) return
+    
     
     // 判断此消息类型是否为文本
-    if (msg.type() == Message.Type.Text) {
-      await msg.say('啊啊啊啊啊')
-    }
-    if (msg.type() == Message.Type.toRecalled) {
-      const recalledMessage = await msg.toRecalled()
-      await msg.say(`Message: ${recalledMessage} has been recalled.`)
+    
+    if (msg.room()) {
+      // 获取群聊
+      const room = await msg.room()
+      // 收到消息，提到自己
+      if (await msg.mentionSelf()) {
+        const index = Math.floor(Math.random() * imgs.length)
+        const randomImg = imgs[index];
+        const fileBox1 = FileBox.fromUrl(
+          'http://59.110.223.199/img/cat.gif',
+          'cat.gif',
+        )
+        console.log('----1----', index, fileBox1)
+        await msg.say(fileBox1)
+        return
+      }
+
+    } else {
+      if (msg.type() == Message.Type.Text) {
+        try {
+          const fileBox = FileBox.fromFile('D:\\studySpace\\myProject\\wechaty\\wechaty-robot\\img\\meme.jpg')
+          console.log('----2----', fileBox)
+          await msg.say(fileBox)
+        } catch (error) {
+          console.log(error)
+        }
+      }
     }
   }
 }
